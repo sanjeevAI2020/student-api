92ed85a9e26420af2b946130ef3f1b9e6fb4f39a
Sanjeev Shah <100677234+Sanjeev2K20@users.noreply.github.com>
2026-06-09 02:37:16 +0530

    Added frontend UI

--- public/script.js
+++ public/script.js
@@ -1,7 +1,33 @@
 async function loadStudents() {
-    const response = await fetch('/students');
-    const data = await response.json();
+  const response = await fetch("/students");
+  const students = await response.json();
 
-    document.getElementById('result').innerHTML =
-        JSON.stringify(data, null, 2);
+  let html = `
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Course</th>
+                <th>Age</th>
+            </tr>
+    `;
+
+  students.forEach((student) => {
+    html += `
+            <tr>
+                <td>${student.id}</td>
+                <td>${student.first_name}</td>
+                <td>${student.last_name}</td>
+                <td>${student.email}</td>
+                <td>${student.course}</td>
+                <td>${student.age}</td>
+            </tr>
+        `;
+  });
+
+  html += `</table>`;
+
+  document.getElementById("result").innerHTML = html;
 }
